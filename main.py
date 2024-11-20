2c8d4877b91037433f4a526c7aa498fce3b42366
aylime31 <121061385+aylime31@users.noreply.github.com>
2024-11-20 19:50:56 +0200

    Identificare directie privire (up, down, left, right)

--- main.py
+++ main.py
@@ -6,39 +6,37 @@ from contextlib import contextmanager
 import time
 from datetime import datetime
 
-#constante pentru vizualizarea si analiza clipirii
+# Constante pentru vizualizare și analiză clipiri
 WINDOW_NAME = 'Debug feed'
 EAR_QUEUE_SIZE = 100
 EAR_DISPLAY_SCALE = 100
 GRAPH_OFFSET_X = 10
 GRAPH_OFFSET_Y = 100
 
-#punctele de reper pentru detectarea ochilor conform modelului MediaPipe Face Mesh
+# Punctele de reper pentru detectarea ochilor conform modelului MediaPipe Face Mesh
 LEFT_EYE_INDICES = np.array([33, 160, 158, 133, 153, 144])
 RIGHT_EYE_INDICES = np.array([362, 385, 387, 263, 373, 380])
+IRIS_CENTER_LEFT = 468
+IRIS_CENTER_RIGHT = 473
 
-#constante pentru detectarea clipirii
+# Constante pentru detectarea clipirii
 BLINK_MIN_DURATION = 0.1  # secunde
 BLINK_MAX_DURATION = 0.4
 BLINK_THRESHOLD_OFFSET = 0.16
 
 
-
 def save_blink_data(blink_data):
     filename = "blink_rate.txt"
     with open(filename, "a") as file:
-
         file.write("Data si Ora\tMinut\tSecunda\tEAR\tNumar Clipiri\tRata Clipiri (clipiri/minut)\n")
         for data in blink_data:
             file.write(
-                f"{data['timestamp']}\t{data['minute']}\t{data['second']}\t{data['ear']}\t{data['blink_count']}\t{data['blink_rate']}\n")
+                f"{data['timestamp']}\t{data['minute']}\t{data['second']}\t{data['ear']}\t{data['blink_count']}\t{data['blink_rate']}\n ")
 
     return filename
 
 
-
 def collect_blink_data(blink_data, blink_detector, ear, timestamp, minute, second):
-    #rata de clipire la fiecare minut
     blink_rate = blink_detector.blink_rate
     blink_data.append({
         "timestamp": timestamp,
@@ -52,7 +50,6 @@ def collect_blink_data(blink_data, blink_detector, ear, timestamp, minute, secon
 
 @contextmanager
 def video_capture(source=0):
-    """Gestionează camera de la deschidere la închidere"""
     cap = cv2.VideoCapture(source)
     try:
         if not cap.isOpened():
@@ -64,7 +61,6 @@ def video_capture(source=0):
 
 
 def calculate_ear_vectorized(landmarks, eye_indices):
-    """Calculează raportul de aspect al ochiului (EAR) cu vectorizare"""
     points = np.array([[landmarks[idx].x, landmarks[idx].y] for idx in eye_indices])
     vert1 = np.linalg.norm(points[1] - points[5])
     vert2 = np.linalg.norm(points[2] - points[4])
@@ -72,18 +68,32 @@ def calculate_ear_vectorized(landmarks, eye_indices):
     return (vert1 + vert2) / (2.0 * horz)
 
 
-def draw_ear_graph(frame, ear_values):
-    """Desenează graficul EAR în timp real folosind operații vectorizate"""
-    if len(ear_values) < 2:
-        return
+def detect_eye_direction(landmarks, left_eye_indices, right_eye_indices, iris_center_left, iris_center_right):
+    left_eye = np.array([[landmarks[idx].x, landmarks[idx].y] for idx in left_eye_indices])
+    right_eye = np.array([[landmarks[idx].x, landmarks[idx].y] for idx in right_eye_indices])
 
+    iris_left = np.array([landmarks[iris_center_left].x, landmarks[iris_center_left].y])
+    iris_right = np.array([landmarks[iris_center_right].x, landmarks[iris_center_right].y])
 
-    indices = np.arange(len(ear_values)) + GRAPH_OFFSET_X
-    values = np.array(ear_values) * EAR_DISPLAY_SCALE
-    y_coords = GRAPH_OFFSET_Y - values.astype(int)
+    left_horizontal_ratio = (iris_left[0] - left_eye[0, 0]) / (left_eye[3, 0] - left_eye[0, 0])
+    left_vertical_ratio = (iris_left[1] - left_eye[1, 1]) / (left_eye[4, 1] - left_eye[1, 1])
 
-    points = np.column_stack((indices, y_coords)).reshape((-1, 1, 2))
-    cv2.polylines(frame, [points], False, (0, 255, 0), 2)
+    right_horizontal_ratio = (iris_right[0] - right_eye[0, 0]) / (right_eye[3, 0] - right_eye[0, 0])
+    right_vertical_ratio = (iris_right[1] - right_eye[1, 1]) / (right_eye[4, 1] - right_eye[1, 1])
+
+    horizontal_ratio = (left_horizontal_ratio + right_horizontal_ratio) / 2
+    vertical_ratio = (left_vertical_ratio + right_vertical_ratio) / 2
+
+    if horizontal_ratio < 0.4:
+        return "Left"
+    elif horizontal_ratio > 0.6:
+        return "Right"
+    elif vertical_ratio < 0.4:
+        return "Up"
+    elif vertical_ratio > 0.6:
+        return "Down"
+    else:
+        return "Center"
 
 
 class BlinkDetector:
@@ -92,8 +102,8 @@ class BlinkDetector:
         self.is_blinking = False
         self.blink_count = 0
         self.last_blink_time = time.time()
-        self.first_blink_time = None  # Timpul primului clipit
-        self.blink_rate = 0.0  # Rata de clipire
+        self.first_blink_time = None
+        self.blink_rate = 0.0
 
     def update(self, ear, threshold):
         current_time = time.time()
@@ -110,26 +120,23 @@ class BlinkDetector:
                     self.last_blink_time = current_time
             self.blink_start = None
 
-        #rata de clipire
         if self.first_blink_time is None:
             self.first_blink_time = current_time
 
-        #verificam daca a trecut un minut de la primul clipit
         elapsed_time = current_time - self.first_blink_time
-        if elapsed_time >= 60:  # 60 secunde
-            self.blink_rate = self.blink_count  # Rata de clipire = numărul de clipiri într-un minut
-            self.first_blink_time = current_time  # Resetează cronometru
-            self.blink_count = 0  # Resetează contorul clipirilor pentru următorul minut
+        if elapsed_time >= 60:
+            self.blink_rate = self.blink_count
+            self.first_blink_time = current_time
+            self.blink_count = 0
 
 
 def process_frame(frame, face_mesh, mp_face_mesh, mp_drawing, mp_drawing_styles, ear_values, running_sum,
-                  blink_detector, blink_data):
+                  blink_detector, blink_data, eye_movement_data):
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = face_mesh.process(rgb_frame)
 
     if results.multi_face_landmarks:
         for face_landmarks in results.multi_face_landmarks:
-
             mp_drawing.draw_landmarks(
                 image=frame,
                 landmark_list=face_landmarks,
@@ -137,21 +144,17 @@ def process_frame(frame, face_mesh, mp_face_mesh, mp_drawing, mp_drawing_styles,
                 landmark_drawing_spec=None,
                 connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style())
 
-            #EAR pentru ambii ochi
             left_ear = calculate_ear_vectorized(face_landmarks.landmark, LEFT_EYE_INDICES)
             right_ear = calculate_ear_vectorized(face_landmarks.landmark, RIGHT_EYE_INDICES)
             ear = (left_ear + right_ear) / 2.0
 
-            #actualizeaza suma rulanta inainte de adaugarea noii valori
             if len(ear_values) == EAR_QUEUE_SIZE:
                 running_sum[0] -= ear_values[0]
             running_sum[0] += ear
             ear_values.append(ear)
 
-            # Folosește suma rulantă pentru medie
             rolling_avg = running_sum[0] / len(ear_values)
             blink_threshold = -0.933 + 11.726 / 9.112 * (1 - np.exp(-9.112 * rolling_avg))
-
 
             blink_detector.update(ear, blink_threshold)
 
@@ -159,47 +162,32 @@ def process_frame(frame, face_mesh, mp_face_mesh, mp_drawing, mp_drawing_styles,
             minute = datetime.now().minute
             second = datetime.now().second
 
-
             collect_blink_data(blink_data, blink_detector, ear, timestamp, minute, second)
+
+            direction = detect_eye_direction(face_landmarks.landmark, LEFT_EYE_INDICES, RIGHT_EYE_INDICES,
+                                             IRIS_CENTER_LEFT, IRIS_CENTER_RIGHT)
+            eye_movement_data[direction] += 1
 
             cv2.putText(frame, f'EAR: {ear:.2f} Avg: {rolling_avg:.2f}', (10, 30),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
             cv2.putText(frame, f'Blinks: {blink_detector.blink_count}', (10, 60),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-
             cv2.putText(frame, f'Blink Rate: {blink_detector.blink_rate} Blinks/min', (10, 90),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-
-
-            avg_line_y = GRAPH_OFFSET_Y - int(rolling_avg * EAR_DISPLAY_SCALE)
-            cv2.line(frame,
-                     (GRAPH_OFFSET_X, avg_line_y),
-                     (GRAPH_OFFSET_X + len(ear_values), avg_line_y),
-                     (0, 0, 255), 2)
-
-
-            threshold_y = GRAPH_OFFSET_Y - int(blink_threshold * EAR_DISPLAY_SCALE)
-            cv2.line(frame,
-                     (GRAPH_OFFSET_X, threshold_y),
-                     (GRAPH_OFFSET_X + len(ear_values), threshold_y),
-                     (255, 0, 0), 2)
-
-            draw_ear_graph(frame, ear_values)
-
+            cv2.putText(frame, f'Eye Direction: {direction}', (10, 120),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
 
 def main():
-    #componentele necesare pentru detectia feței si analiza ochilor
     mp_face_mesh = mp.solutions.face_mesh
-    face_mesh = mp_face_mesh.FaceMesh()
+    face_mesh = mp_face_mesh.FaceMesh(refine_landmarks=True)  # Refine landmarks to include iris points
     mp_drawing = mp.solutions.drawing_utils
     mp_drawing_styles = mp.solutions.drawing_styles
-    ear_values = deque(maxlen=EAR_QUEUE_SIZE)  # coada pentru grafic
-    running_sum = [0.0]  #suma rulanta pentru medie
+    ear_values = deque(maxlen=EAR_QUEUE_SIZE)
+    running_sum = [0.0]
     blink_detector = BlinkDetector()
-
-    #lista pentru a stoca datele de clipire
     blink_data = []
+    eye_movement_data = {"Left": 0, "Right": 0, "Up": 0, "Down": 0, "Center": 0}
 
     with video_capture(source=0) as cap:
         while True:
@@ -209,7 +197,7 @@ def main():
                 break
 
             process_frame(frame, face_mesh, mp_face_mesh, mp_drawing,
-                          mp_drawing_styles, ear_values, running_sum, blink_detector, blink_data)
+                          mp_drawing_styles, ear_values, running_sum, blink_detector, blink_data, eye_movement_data)
             cv2.imshow(WINDOW_NAME, frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
