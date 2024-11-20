982b083f5226533d4bc8a5cfa8a5d65250db0bbc
aylime31 <121061385+aylime31@users.noreply.github.com>
2024-11-20 12:56:40 +0200

    Calcul rata de clipire si stocare date in fisier txt

--- main.py
+++ main.py
@@ -4,22 +4,50 @@ import numpy as np
 from collections import deque
 from contextlib import contextmanager
 import time
+from datetime import datetime
 
-# Constante pentru vizualizarea și analiza clipirii
+#constante pentru vizualizarea si analiza clipirii
 WINDOW_NAME = 'Debug feed'
 EAR_QUEUE_SIZE = 100
 EAR_DISPLAY_SCALE = 100
 GRAPH_OFFSET_X = 10
 GRAPH_OFFSET_Y = 100
 
-# Punctele de reper pentru detectarea ochilor conform modelului MediaPipe Face Mesh
+#punctele de reper pentru detectarea ochilor conform modelului MediaPipe Face Mesh
 LEFT_EYE_INDICES = np.array([33, 160, 158, 133, 153, 144])
 RIGHT_EYE_INDICES = np.array([362, 385, 387, 263, 373, 380])
 
-# Constante pentru detectarea clipirii
-BLINK_MIN_DURATION = 0.1 # secunde
+#constante pentru detectarea clipirii
+BLINK_MIN_DURATION = 0.1  # secunde
 BLINK_MAX_DURATION = 0.4
 BLINK_THRESHOLD_OFFSET = 0.16
+
+
+
+def save_blink_data(blink_data):
+    filename = "blink_rate.txt"
+    with open(filename, "a") as file:
+
+        file.write("Data si Ora\tMinut\tSecunda\tEAR\tNumar Clipiri\tRata Clipiri (clipiri/minut)\n")
+        for data in blink_data:
+            file.write(
+                f"{data['timestamp']}\t{data['minute']}\t{data['second']}\t{data['ear']}\t{data['blink_count']}\t{data['blink_rate']}\n")
+
+    return filename
+
+
+
+def collect_blink_data(blink_data, blink_detector, ear, timestamp, minute, second):
+    #rata de clipire la fiecare minut
+    blink_rate = blink_detector.blink_rate
+    blink_data.append({
+        "timestamp": timestamp,
+        "minute": minute,
+        "second": second,
+        "ear": round(ear, 2),
+        "blink_count": blink_detector.blink_count,
+        "blink_rate": blink_rate
+    })
 
 
 @contextmanager
@@ -48,12 +76,12 @@ def draw_ear_graph(frame, ear_values):
     """Desenează graficul EAR în timp real folosind operații vectorizate"""
     if len(ear_values) < 2:
         return
-    
-    # Precalculează punctele folosind operații numpy
+
+
     indices = np.arange(len(ear_values)) + GRAPH_OFFSET_X
     values = np.array(ear_values) * EAR_DISPLAY_SCALE
     y_coords = GRAPH_OFFSET_Y - values.astype(int)
-    
+
     points = np.column_stack((indices, y_coords)).reshape((-1, 1, 2))
     cv2.polylines(frame, [points], False, (0, 255, 0), 2)
 
@@ -64,10 +92,12 @@ class BlinkDetector:
         self.is_blinking = False
         self.blink_count = 0
         self.last_blink_time = time.time()
+        self.first_blink_time = None  # Timpul primului clipit
+        self.blink_rate = 0.0  # Rata de clipire
 
     def update(self, ear, threshold):
         current_time = time.time()
-        
+
         if not self.is_blinking and ear < threshold:
             self.is_blinking = True
             self.blink_start = current_time
@@ -80,14 +110,26 @@ class BlinkDetector:
                     self.last_blink_time = current_time
             self.blink_start = None
 
+        #rata de clipire
+        if self.first_blink_time is None:
+            self.first_blink_time = current_time
 
-def process_frame(frame, face_mesh, mp_face_mesh, mp_drawing, mp_drawing_styles, ear_values, running_sum, blink_detector):
+        #verificam daca a trecut un minut de la primul clipit
+        elapsed_time = current_time - self.first_blink_time
+        if elapsed_time >= 60:  # 60 secunde
+            self.blink_rate = self.blink_count  # Rata de clipire = numărul de clipiri într-un minut
+            self.first_blink_time = current_time  # Resetează cronometru
+            self.blink_count = 0  # Resetează contorul clipirilor pentru următorul minut
+
+
+def process_frame(frame, face_mesh, mp_face_mesh, mp_drawing, mp_drawing_styles, ear_values, running_sum,
+                  blink_detector, blink_data):
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = face_mesh.process(rgb_frame)
 
     if results.multi_face_landmarks:
         for face_landmarks in results.multi_face_landmarks:
-            # Desenează reperele faciale pt debugging
+
             mp_drawing.draw_landmarks(
                 image=frame,
                 landmark_list=face_landmarks,
@@ -95,38 +137,47 @@ def process_frame(frame, face_mesh, mp_face_mesh, mp_drawing, mp_drawing_styles,
                 landmark_drawing_spec=None,
                 connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style())
 
-            # Calculează EAR pentru ambii ochi
+            #EAR pentru ambii ochi
             left_ear = calculate_ear_vectorized(face_landmarks.landmark, LEFT_EYE_INDICES)
             right_ear = calculate_ear_vectorized(face_landmarks.landmark, RIGHT_EYE_INDICES)
             ear = (left_ear + right_ear) / 2.0
-            
-            # Actualizează suma rulantă înainte de adăugarea noii valori
+
+            #actualizeaza suma rulanta inainte de adaugarea noii valori
             if len(ear_values) == EAR_QUEUE_SIZE:
                 running_sum[0] -= ear_values[0]
             running_sum[0] += ear
             ear_values.append(ear)
-            
+
             # Folosește suma rulantă pentru medie
             rolling_avg = running_sum[0] / len(ear_values)
-            blink_threshold = -0.933 + 11.726/9.112 * (1 - np.exp(-9.112 * rolling_avg)) # todo: altă formulă
-            
-            # Actualizează detecția clipirii
+            blink_threshold = -0.933 + 11.726 / 9.112 * (1 - np.exp(-9.112 * rolling_avg))
+
+
             blink_detector.update(ear, blink_threshold)
 
-            # Afișează valorile
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            minute = datetime.now().minute
+            second = datetime.now().second
+
+
+            collect_blink_data(blink_data, blink_detector, ear, timestamp, minute, second)
+
             cv2.putText(frame, f'EAR: {ear:.2f} Avg: {rolling_avg:.2f}', (10, 30),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
             cv2.putText(frame, f'Blinks: {blink_detector.blink_count}', (10, 60),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-            # Desenează media rulante (todo: ignoră valorile EAR la clipiri)
+            cv2.putText(frame, f'Blink Rate: {blink_detector.blink_rate} Blinks/min', (10, 90),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+
             avg_line_y = GRAPH_OFFSET_Y - int(rolling_avg * EAR_DISPLAY_SCALE)
             cv2.line(frame,
                      (GRAPH_OFFSET_X, avg_line_y),
                      (GRAPH_OFFSET_X + len(ear_values), avg_line_y),
                      (0, 0, 255), 2)
 
-            # Desenează threshold-ul
+
             threshold_y = GRAPH_OFFSET_Y - int(blink_threshold * EAR_DISPLAY_SCALE)
             cv2.line(frame,
                      (GRAPH_OFFSET_X, threshold_y),
@@ -136,17 +187,20 @@ def process_frame(frame, face_mesh, mp_face_mesh, mp_drawing, mp_drawing_styles,
             draw_ear_graph(frame, ear_values)
 
 
+
 def main():
-    # Inițializează componentele necesare pentru detecția feței și analiza ochilor
+    #componentele necesare pentru detectia feței si analiza ochilor
     mp_face_mesh = mp.solutions.face_mesh
     face_mesh = mp_face_mesh.FaceMesh()
     mp_drawing = mp.solutions.drawing_utils
     mp_drawing_styles = mp.solutions.drawing_styles
-    ear_values = deque(maxlen=EAR_QUEUE_SIZE)  # coadă pentru grafic
-    running_sum = [0.0]  # Folosim listă pentru referință mutabilă
+    ear_values = deque(maxlen=EAR_QUEUE_SIZE)  # coada pentru grafic
+    running_sum = [0.0]  #suma rulanta pentru medie
     blink_detector = BlinkDetector()
 
-    # Bucla principală pentru procesarea prin cameră (todo: compatibilitate pentru input video)
+    #lista pentru a stoca datele de clipire
+    blink_data = []
+
     with video_capture(source=0) as cap:
         while True:
             ret, frame = cap.read()
@@ -154,12 +208,14 @@ def main():
                 print("Eroare: Nu s-a putut citi cadrul.")
                 break
 
-            process_frame(frame, face_mesh, mp_face_mesh, mp_drawing, 
-                        mp_drawing_styles, ear_values, running_sum, blink_detector)
+            process_frame(frame, face_mesh, mp_face_mesh, mp_drawing,
+                          mp_drawing_styles, ear_values, running_sum, blink_detector, blink_data)
             cv2.imshow(WINDOW_NAME, frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+
+    save_blink_data(blink_data)
 
 
 if __name__ == '__main__':
